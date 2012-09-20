6cb56e10be61bcae22dffe31e0da0cbc714ac375
Jonas Wagner <jonas.wagner@epfl.ch>
2012-09-20 12:52:58 +0200

    Add Test project.

---
+++ Sweng2012QuizAppTestTest/src/epfl/sweng/test/ShowQuestionsActivityTest.java
@@ -0,0 +1,21 @@
+package epfl.sweng.test;
+
+import epfl.sweng.showquestions.ShowQuestionsActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class ShowQuestionsActivityTest extends
+		ActivityInstrumentationTestCase2<ShowQuestionsActivity> {
+
+	public ShowQuestionsActivityTest() {
+		super(ShowQuestionsActivity.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	public void testHello() {
+		fail("Not yet implemented");
+	}
+
+}
